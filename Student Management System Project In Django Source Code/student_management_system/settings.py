c29e2c7827c0c7dbf04d631ce54e8e0594c9ca43
Shubhamvarade65 <138111051+Shubhamvarade65@users.noreply.github.com>
2024-09-26 16:56:19 +0530

    Update settings.py

--- Student Management System Project In Django Source Code/student_management_system/settings.py
+++ Student Management System Project In Django Source Code/student_management_system/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'student_management',  # your database name
         'USER': 'root',                # your MySQL username
-        'PASSWORD': 'your_password',   # your MySQL password
+        'PASSWORD': 'studentmanagement',   # your MySQL password
         'HOST': 'localhost',           # or '127.0.0.1'
         'PORT': '3306',                # default MySQL port
     }
